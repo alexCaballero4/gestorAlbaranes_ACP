15868a3f24bd093eb3882bb12dfbae76d97ada33
alexCaballero4 <alexcaballero846@gmail.com>
2024-12-09 19:11:33 +0100

    Create Footer.jsx

---
+++ src/app/components/ui/Footer.jsx
@@ -0,0 +1,10 @@
+import '@/app/css/footer.css';
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2024 Gestión de Albaranes</p>
+      <p>Creado por Alejandro Caballero</p>
+    </footer>
+  );
+}
